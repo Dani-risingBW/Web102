527242c21af6ebc4a8183d3201cf67142689f09e
Nkiruka Ibe <nkirukaibe05@hotmail.com>
2025-06-21 20:17:44 -0400

    Completed Lab 3 - Barista App

---
+++ barista-app/src/Components/RecipeChoices.jsx
@@ -0,0 +1,25 @@
+import React, { Component, useEffect, useState } from "react";
+
+const RecipeChoices = ({ handleChange, label, choices, currentVal }) => {
+    return (
+        <div className="radio-buttons">
+             <input
+                type="text"
+                name={label}
+                value={currentVal}
+                placeholder="Guess the ingredient..."
+                onChange={handleChange}
+                className="textbox"
+            />
+            {choices &&
+                choices.map((choice) => (
+                    <li key={choice}>
+                        {choice}
+                    </li>
+            ))}
+           
+        </div>
+    );
+};
+
+export default RecipeChoices;
