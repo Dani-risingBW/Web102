029873ed75ccd8e87b9380bccba88d450cd35e28
Nkiruka Ibe <nkirukaibe05@hotmail.com>
2025-07-17 21:35:02 -0400

    Completed and updated version of Lab 5 with stretch features

---
+++ CryptoHustle-pro/src/components/SideNav.jsx
@@ -0,0 +1,14 @@
+import CryptoNews from "./CryptoNews"
+import React from "react"
+
+function SideNav () {
+    return (
+        <div className="sidenav">
+            <h1>Nav bar</h1>
+            <CryptoNews />
+        </div>
+    )
+
+}
+
+export default SideNav;
