08b7f8a8392dff99695718c54b0577a9bbf5127a
Nkiruka Ibe <nkirukaibe05@hotmail.com>
2025-07-29 14:56:38 -0400

    Lab 7 - React CRUD Application

---
+++ web102_unit7lab-main/src/pages/CreatePost.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './CreatePost.css'
+import { supabase } from '../client'
+
+
+const CreatePost = () => {
+    const createPost = async (event) => {
+        event.preventDefault()
+        // Create a new post in the database
+        const { error } = await supabase
+        .from('Posts')
+        .insert({ title: post.title, author: post.author, description: post.description });
+        if (error) {
+            alert("Error: " + error.message);
+            return;
+        }
+        window.location = "/";
+    }
+    const [post, setPost] = useState({title: "", author: "", description: ""})
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="title">Title</label> <br />
+                <input type="text" id="title" name="title" onChange={handleChange} /><br />
+                <br/>
+
+                <label htmlFor="author">Author</label><br />
+                <input type="text" id="author" name="author" onChange={handleChange} /><br />
+                <br/>
+
+                <label htmlFor="description">Description</label><br />
+                <textarea rows="5" cols="50" id="description" name="description" onChange={handleChange}>
+                </textarea>
+                <br/>
+                <input type="submit" value="Submit" onClick={createPost} />
+            </form>
+        </div>
+    )
+}
+
+export default CreatePost
